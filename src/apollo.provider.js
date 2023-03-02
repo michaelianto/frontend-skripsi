a0c4f3284e96e59819b4a73f7eb168788830e817
Clementius Tanzil <clementius.tanzil@gdn-commerce.com>
2023-03-02 17:21:48 +0700

    add validation and change port

--- src/apollo.provider.js
+++ src/apollo.provider.js
@@ -1,13 +1,13 @@
-import { InMemoryCache, ApolloClient } from '@apollo/client'
-import { createApolloProvider } from '@vue/apollo-option'
+import { InMemoryCache, ApolloClient } from "@apollo/client";
+import { createApolloProvider } from "@vue/apollo-option";
 
-const cache = new InMemoryCache()
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  cache: cache,
-  uri: "http://localhost:8080/graphql"
-})
+	cache: cache,
+	uri: "http://localhost:8090/graphql",
+});
 
 export const provider = createApolloProvider({
-  defaultClient: client
-})
+	defaultClient: client,
+});
